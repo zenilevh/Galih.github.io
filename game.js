30161728e04f480e2e8bc36d566857b737c5a2ea
Galih.H <zenilevh@gmail.com>
2020-06-05 15:45:20 +0700

    final UPDATE

--- game.js
+++ game.js
@@ -14,79 +14,115 @@ let tutorialCLick = document.getElementById("tutorial");
 
 // function dragonDamage() {
 //     //bikin Dulu hp nya
-//     const dragonHP = document.getElementById("dragonHP");
-//     const dragonFullHP = dragonHP.innerHTML("FULL HEALTH");
-//     const dragonHalfHP = dragonHP.innerHTML("50% HEALTH");
-//     const dragonLowHP = dragonHP.innerHTML("10% HEALTH");
-// }
+const dragonHP = document.getElementById("dragonHP");
+// variable asset
+let stringTimer = document.getElementById("stringTimer");
+let optionSurrender = document.getElementById("surrender");
+let warning = document.getElementById("dragonWarning");
 
 tutorialCLick.addEventListener("click", function() {
     countDownTimer();
     showCard();
-    bgm.play();
     randomCard();
+    // bgm.play();
+    dragonHP.innerHTML = "FULL HEALTH";
     alert(
         "You have to pick one from many card, and only the right card to get you win"
     );
 });
+
 //untuk munculin kartu dan random kartunya
-let card = document.getElementsByClassName("card");
+let cards = document.getElementsByClassName("card");
 
 function showCard() {
-    for (let i = 0; i < card.length; i++) {
-        card[
+    for (let i = 0; i < cards.length; i++) {
+        cards[
             i
         ].innerHTML = `<img src="https://i.ya-webdesign.com/images/pixel-knight-png-19.gif">`;
     }
 }
+let name = document.getElementsByClassName("dataName");
 
-//random kartu
+//random kartu dan assign waktu
+
+let seconds;
+let truecard;
+
 function randomCard() {
-    let cardIndex = Math.ceil(Math.random() * 10);
-    let randomcard = document.getElementsByClassName(cardIndex.toString());
+    let cardIndex = Math.ceil(Math.random() * 7);
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].dataset.correct = false;
+    }
+    truecard = document.getElementsByClassName(cardIndex.toString())[0];
+    truecard.dataset.correct = true;
+    console.log(truecard);
 }
 
-let name = document.getElementsByClassName("dataName");
-console.log(name);
+document.getElementById("trap").addEventListener("click", function(event) {
+    console.log(event.target);
+});
+let trueCounter = 0;
+let falseCounter = 0;
 
-document.getElementById(
-    "trap".addEventListener("click", function(event) {
-        let selectedElement = event.target;
-    })
-);
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function(event) {
+        if (cards[i].dataset.correct === "true") {
+            //kalo kondisi card yang di klik itu true
+            console.log("ini kartu bener", truecard);
+            showCard();
+            randomCard();
+            trueCounter++;
+            console.log(trueCounter);
+            if (trueCounter === 1) {
+                dragonHP.innerHTML = "50% HEALTH";
+            }
+            if (trueCounter == 2 && dragonHP.innerHTML == "50% HEALTH") {
+                dragonHP.innerHTML = "10% HEALTH";
+            }
+            if (trueCounter == 3 && dragonHP.innerHTML == "10% HEALTH") {
+                stringTimer.innerHTML = "DRAGON DEATH";
+                optionSurrender.innerHTML = "DRAGON DEATH";
+                warning.innerHTML = "DRAGON DEATH";
+                alert("YOU WIN, PLEASE GO TO THE GOLD CHAMBER");
+                window.location.href = `./tamat.html`;
+            }
+        } else {
+            console.log("ini kartu salah");
+            //kalo kondisi nya false
 
-//     randomcard[0].innerHTML = `<a href='tamat.html'><img src="https://i.ya-webdesign.com/images/pixel-knight-png-19.gif"></a>`;
-//     // randomcard[0].dataset.correct = true;
-//     // let selectedElement;
-//     // randomcard[0].dataset.correct = true;
-//     // if (selectedElement.dataset.correct === "true") {
-//     //     dragonFullHP;
-//     // }
-// }
+            alert("YOU PICK WRONG CARD, YOU ARE DEAD");
+            window.location.href = `./index.html`;
+            seconds = 0;
+            falseCounter++;
+        }
+    });
+}
 
-//untuk timer nya
+//untuk timer nya dan kondisi
 
 function countDownTimer() {
-    var seconds = 10;
-
+    let seconds = 30;
     var id = setInterval(function() {
         seconds--;
         document.getElementById("timer").innerHTML = `${seconds} s`;
-        let stringTimer = document.getElementById("stringTimer");
-        stringTimer.style =
+        console.log(seconds);
 
-            if (seconds == 0) {
-                clearInterval(id);
-                seconds = 0;
-                stringTimer.innerHTML = "`YOU ARE DEAD, INSANT KILL FROM DRAGON`";
-                document.getElementById("timer").innerHTML = ` `;
-                bgm.pause();
-                randomcard();
-                for (let i = 0; i < card.length; i++) {
-                    card[i].innerHTML = ` `;
-                }
+        if (seconds == 0) {
+            clearInterval(id);
+            seconds = 0;
+            // kondisi tambahan jika waktu sentuh angka 0
+            warning.innerHTML = "YOU ARE SO WEAK HUMAN, GIVE ME ANOTHER CHALLENGER!!";
+            stringTimer.innerHTML = "`YOU ARE DEAD, INSANT KILL FROM DRAGON`";
+            stringTimer.style.color = "#ff6e6e";
+            optionSurrender.innerHTML = "YOU ARE DEAD, PLEASE GIMME NEW CHALLENGER";
+            tutorialCLick.innerHTML = "";
+            document.getElementById("timer").innerHTML = ` `;
+            bgm.pause();
+            bgm.remove();
+            dead.play();
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].innerHTML = ``;
             }
+        }
     }, 1000);
 }
-
-function heroGetDamage() {}
